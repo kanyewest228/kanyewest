ffb667cdf45bcdca2893dda01bfacb776df346b7
kanyewest <pavlovskidog@gmail.com>
2025-10-15 12:44:49 +0700

    with delay (??)

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 function startGame() {
-
-    const cardsImage = ['img/crab.jpg', 'img/dolphin.jpg', 'img/fooga.jpg', 'img/medusa.jpg', 'img/octopus.jpg',  'img/shark.jpg', 'img/whale.jpg', 'img/zola.jpg']
+    document.getElementById('parent').innerHTML = '';
+    const cardsImage = ['img/crab.jpg', 'img/dolphin.jpg', 'img/fooga.jpg', 'img/medusa.jpg', 'img/octopus.jpg', 'img/shark.jpg', 'img/whale.jpg', 'img/zola.jpg']
     const cardsShuffle = [...cardsImage, ...cardsImage].sort(() => Math.random() - 0.5)
 
     let cardDiv = [];
     let parent = document.getElementById('parent')
-    let deckBlocked = false
-    let flippedCards = []
 
     while (cardsShuffle.length > 0) {
         cardDiv = parent.appendChild(document.createElement('div'))
@@ -26,53 +24,47 @@ function startGame() {
         backImg.classList.add = 'backImg'
         backImg.setAttribute('draggable', 'false')
 
-
         cardDiv.addEventListener('click', flipCard)
 
         cardsShuffle.shift()
     }
+}
 
+let deckBlocked = false
+let flippedCards = []
 
-    function flipCard () {
-        if (deckBlocked) return
-        if (this === flippedCards[0]) return
-        if (this.classList.contains('matched')) return
+function flipCard () {
+    if (deckBlocked) return
+    if (this === flippedCards[0]) return
+    if (this.classList.contains('matched')) return
+    this.firstChild.classList.toggle('hid')
+    this.lastChild.classList.toggle('hid')
+    flippedCards.push(this)
 
-        this.firstChild.classList.toggle('hid')
-        this.lastChild.classList.toggle('hid')
-        flippedCards.push(this)
-
-        if (flippedCards.length === 2) {
-            checkMatch()
-        }
-    }
-
-    function checkMatch () {
-        deckBlocked = true;
-        setTimeout(() => {
-            const card1 = flippedCards[0]
-            const card2 = flippedCards[1]
-
-            if(card1.firstChild.src === card2.firstChild.src) {
-                console.log("MATCH")
-                card1.removeEventListener('click', flipCard)
-                card2.removeEventListener('click', flipCard)
-            } else {
-                card1.firstChild.classList.toggle('hid')
-                card1.lastChild.classList.toggle('hid')
-                card2.firstChild.classList.toggle('hid')
-                card2.lastChild.classList.toggle('hid')
-            }
-
-            flippedCards = []
-            deckBlocked = false
-        }, 1000)
-
-
+    if (flippedCards.length === 2) {
+        checkMatch()
     }
 }
 
+function checkMatch () {
+    deckBlocked = true;
+    setTimeout(() => {
+        const card1 = flippedCards[0]
+        const card2 = flippedCards[1]
 
+        if(card1.firstChild.src === card2.firstChild.src) {
+            console.log("MATCH")
+            card1.removeEventListener('click', flipCard)
+            card2.removeEventListener('click', flipCard)
+        } else {
+            card1.firstChild.classList.toggle('hid')
+            card1.lastChild.classList.toggle('hid')
+            card2.firstChild.classList.toggle('hid')
+            card2.lastChild.classList.toggle('hid')
+        }
 
+        flippedCards = []
+        deckBlocked = false
+    }, 1000)
 
-
+}
